677fe4999fb5718573b565a08a25c94e07750ecf
arihantpandey3724-code <arihantpandey3724@gmail.com>
2026-06-30 00:47:06 +0530

    Info: leaked more private info

--- variables.js
+++ variables.js
@@ -1,7 +1,15 @@
 name = "Arihant Pandey";
 age = 20;
 wealth = "INT_MIN";
+skills = null;
+dreams = undefined;
+marital_status = "Yo yo yo, why u wanna know?";
+sleeping = true;
 
 console.log("NAME: ",name);
 console.log("Age: ",age);
 console.log("Wealth",wealth);
+console.log("Skills: ",skills);
+console.log("My dreams: ",dreams);
+console.log("issleeping: ",sleeping);
+console.log("Married: ",marital_status);
