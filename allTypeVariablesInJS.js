dcbafd3e1e9e1ce93765a61da2e7e806b1e8f318
arihantpandey3724-code <arihantpandey3724@gmail.com>
2026-06-30 01:19:31 +0530

    Explore: All datatypes in JS

---
+++ allTypeVariablesInJS.js
@@ -0,0 +1,20 @@
+let number = 123;
+console.log("This is a num: ",number);
+console.log("Typeof number: ",typeof number, '\n');
+
+let bool = true;
+console.log("The boolean variable is set to: ",bool);
+console.log("Typeof bool",typeof bool, '\n');
+
+let str = "Hello World!!";
+console.log("str contains: ",str);
+console.log("Typeof str: ",typeof str, '\n');
+
+let undef;
+console.log("Variable undef is: ",undef);
+console.log("Typeof undef: ",typeof undef, '\n');
+
+let obj = null;
+console.log("Variable obj is:", obj);
+console.log("Typeof obj:",typeof obj,'\nThere are two other datatypes in JS, namely "BigInt" and "Symbol"');
+
