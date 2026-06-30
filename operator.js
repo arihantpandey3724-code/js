25ef873c3f503692af26e0a9025df2c5f06704aa
arihantpandey3724-code <arihantpandey3724@gmail.com>
2026-06-30 16:42:57 +0530

    Learn: Comments and operators

---
+++ operator.js
@@ -0,0 +1,22 @@
+console.log("All arithematic operators in JS \n\n\n");
+let a = 5;
+let b = 3;
+console.log("a = ", a);
+console.log("b = ", b);
+console.log("\nThis adds a and b: ", a + b);
+console.log("\nThis subtracts a and b: ", a - b);
+console.log("\nThis multiplies a and b: ", a * b);
+console.log("\nThis divides a and b: ", a / b);
+console.log("\nThis is modulo operator: a % b = ", a % b);
+console.log("a raised to the power b (**): ", a ** b);
+
+
+console.log("Unary operators are same as in C");
+
+console.log('Comparators are also same as in C except "==" and "==="');
+
+console.log("== compares the value even if the datatype is different \n");
+console.log('"===" and "!==" first compares the data type after that compares the value\n');
+
+
+console.log("Conditional statement same as in C\n\nEven ternary statement are same as in C");
