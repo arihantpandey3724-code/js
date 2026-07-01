92d37401111e0cb2b5a34f1ac62d5c359fa49c76
arihantpandey3724-code <arihantpandey3724@gmail.com>
2026-07-01 17:43:53 +0530

    Update

--- operator.js
+++ operator.js
@@ -19,4 +19,4 @@ console.log("== compares the value even if the datatype is different \n");
 console.log('"===" and "!==" first compares the data type after that compares the value\n');
 
 
-console.log("Conditional statement same as in C\n\nEven ternary statement are same as in C");
+console.log("Conditional statement same as in C\n\nEven ternary statement and switch cases are same as in C");
